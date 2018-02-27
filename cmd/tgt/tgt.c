2df64c22add75acfcf38e0b201adc60f9531ca95
Jan Kryl <jan.kryl@cloudbyte.com>
2018-02-27 11:57:20 +0100

    [ZOL#23] Replace closed source copyrights in ZoL repository

--- cmd/tgt/tgt.c
+++ cmd/tgt/tgt.c
@@ -1,20 +1,26 @@
 /*
- * ***************************************************************************
- *  (C) Copyright 2017 CloudByte, Inc.
- *  All Rights Reserved.
+ * CDDL HEADER START
  *
- *  This program is an unpublished copyrighted work which is proprietary
- *  to CloudByte, Inc. and contains confidential information that is not
- *  to be reproduced or disclosed to any other person or entity without
- *  prior written consent from CloudByte, Inc. in each and every instance.
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
- *  WARNING:  Unauthorized reproduction of this program as well as
- *  unauthorized preparation of derivative works based upon the
- *  program or distribution of copies by sale, rental, lease or
- *  lending are violations of federal copyright laws and state trade
- *  secret laws, punishable by civil and criminal penalties.
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
  *
- * ***************************************************************************
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright (c) 2017 CloudByte, Inc. All rights reserved.
  */
 
 #include <stdio.h>
