5c1b54bc9351cfd75505780d72e6de5cae87db4f
Mayank <33252549+mynktl@users.noreply.github.com>
2018-05-04 16:58:26 +0530

    ztest crash while running in parallel, and other travis improvements
    - changes related pool_name to be unique
    - doing labelclear + zeroing initial 100 MB of disk/file
    - avoid ztest to run in parallel
    - fixing importargs while importing from API
    - reshuffling a test case to reduce travis time and memory consumption
    
    Signed-off-by: mayank <mayank.patel@cloudbyte.com>

--- cmd/uzfs_test/uzfs_test_sync.sh
+++ cmd/uzfs_test/uzfs_test_sync.sh
@@ -24,63 +24,63 @@ run_sync_test()
 
 	#sync:standard log:yes
 	echo "running sync test with sync:standard, bs:4k and log device"
-	log_must setup_uzfs_test log 4096 $UZFS_TEST_VOLSIZE standard uzfs_sync_pool1 \
-		    uzfs_sync_vol1 uzfs_test_sync_vdev1 uzfs_test_sync_log1
-	log_must export_pool uzfs_sync_pool1
+	log_must setup_uzfs_test log 4096 $UZFS_TEST_VOLSIZE standard uzfs_sync_pool2 \
+		    uzfs_sync_vol2 uzfs_test_sync_vdev2 uzfs_test_sync_log2
+	log_must export_pool uzfs_sync_pool2
 	for i in {1..10}
 	do
-		sudo $UZFS_TEST -S -l -p uzfs_sync_pool1 -d uzfs_sync_vol1 -T 1 -w | grep uzfs_sync_data > $TMPDIR/uzfs_sync_data
+		sudo $UZFS_TEST -S -l -p uzfs_sync_pool2 -d uzfs_sync_vol2 -T 1 -w | grep uzfs_sync_data > $TMPDIR/uzfs_sync_data
 		if [ $? != 0 ]; then
 			return 1;
 		fi
 
-		sudo $UZFS_TEST -T 1 -p uzfs_sync_pool1 -d uzfs_sync_vol1 \
+		sudo $UZFS_TEST -T 1 -p uzfs_sync_pool2 -d uzfs_sync_vol2 \
 		    -V `awk '{print $2}' $TMPDIR/uzfs_sync_data` -m `awk '{print $3}' $TMPDIR/uzfs_sync_data`
 		if [ $? != 0 ]; then
 			return 1;
 		fi
 	done
-	cleanup_uzfs_test uzfs_sync_pool1 uzfs_test_sync_vdev1 uzfs_test_sync_log1
+	cleanup_uzfs_test uzfs_sync_pool2 uzfs_test_sync_vdev2 uzfs_test_sync_log2
 
 	#sync:standard log:false
 	echo "running sync test with sync:standard, bs:64k and no log device"
-	log_must setup_uzfs_test nolog 65536 $UZFS_TEST_VOLSIZE standard uzfs_sync_pool1 \
-	    uzfs_sync_vol1 uzfs_test_sync_vdev1
-	log_must export_pool uzfs_sync_pool1
+	log_must setup_uzfs_test nolog 65536 $UZFS_TEST_VOLSIZE standard uzfs_sync_pool3 \
+	    uzfs_sync_vol3 uzfs_test_sync_vdev3
+	log_must export_pool uzfs_sync_pool3
 	for i in {1..10}
 	do
-		sudo $UZFS_TEST -S -i 8192 -p uzfs_sync_pool1 -d uzfs_sync_vol1 -b 65536 -T 1 -w | grep uzfs_sync_data > $TMPDIR/uzfs_sync_data
+		sudo $UZFS_TEST -S -i 8192 -p uzfs_sync_pool3 -d uzfs_sync_vol3 -b 65536 -T 1 -w | grep uzfs_sync_data > $TMPDIR/uzfs_sync_data
 		if [ $? != 0 ]; then
 			return 1;
 		fi
 
-		sudo $UZFS_TEST -i 8192 -b 65536 -T 1 -p uzfs_sync_pool1 -d uzfs_sync_vol1 \
+		sudo $UZFS_TEST -i 8192 -b 65536 -T 1 -p uzfs_sync_pool3 -d uzfs_sync_vol3 \
 		    -V `awk '{print $2}' $TMPDIR/uzfs_sync_data` -m `awk '{print $3}' $TMPDIR/uzfs_sync_data`
 		if [ $? != 0 ]; then
 			return 1;
 		fi
 	done
-	cleanup_uzfs_test uzfs_sync_pool1 uzfs_test_sync_vdev1
+	cleanup_uzfs_test uzfs_sync_pool3 uzfs_test_sync_vdev3
 
 	#sync:standard log:true
 	echo "running sync test with sync:standard, bs=64k and log device"
-	log_must setup_uzfs_test log 65536 $UZFS_TEST_VOLSIZE standard uzfs_sync_pool1 \
-	    uzfs_sync_vol1 uzfs_test_sync_vdev1 uzfs_test_sync_log1
-	log_must export_pool uzfs_sync_pool1
+	log_must setup_uzfs_test log 65536 $UZFS_TEST_VOLSIZE standard uzfs_sync_pool4 \
+	    uzfs_sync_vol4 uzfs_test_sync_vdev4 uzfs_test_sync_log4
+	log_must export_pool uzfs_sync_pool4
 	for i in {1..10}
 	do
-		sudo $UZFS_TEST -S -i 8192 -b 65536 -p uzfs_sync_pool1 -d uzfs_sync_vol1 -l -T 1 -w | grep uzfs_sync_data > $TMPDIR/uzfs_sync_data
+		sudo $UZFS_TEST -S -i 8192 -b 65536 -p uzfs_sync_pool4 -d uzfs_sync_vol4 -l -T 1 -w | grep uzfs_sync_data > $TMPDIR/uzfs_sync_data
 		if [ $? != 0 ]; then
 			return 1;
 		fi
 
-		sudo $UZFS_TEST -i 8192 -b 65536 -T 1 -p uzfs_sync_pool1 -d uzfs_sync_vol1 \
+		sudo $UZFS_TEST -i 8192 -b 65536 -T 1 -p uzfs_sync_pool4 -d uzfs_sync_vol4 \
 		    -V `awk '{print $2}' $TMPDIR/uzfs_sync_data` -m `awk '{print $3}' $TMPDIR/uzfs_sync_data`
 		if [ $? != 0 ]; then
 			return 1;
 		fi
 	done
-	cleanup_uzfs_test uzfs_sync_pool1 uzfs_test_sync_vdev1 uzfs_test_sync_log1
+	cleanup_uzfs_test uzfs_sync_pool4 uzfs_test_sync_vdev4 uzfs_test_sync_log4
 
 	rm $TMPDIR/uzfs_sync_data
 	return 0
