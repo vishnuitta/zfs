6c25671fd0eb0546f97874eae0ddd50787decdaa
Vishnu Itta <vitta@mayadata.io>
2018-09-24 07:33:42 +0530

    assert in travis
    Signed-off-by: Vishnu Itta <vitta@mayadata.io>

--- include/sys/uzfs_zvol.h
+++ include/sys/uzfs_zvol.h
@@ -96,7 +96,8 @@ typedef struct zvol_state zvol_state_t;
 
 #define	ZVOL_IS_DEGRADED(zv)	(zv->zv_status == ZVOL_STATUS_DEGRADED)
 #define	ZVOL_IS_REBUILDING(zv)		\
-	(zv->rebuild_info.zv_rebuild_status == ZVOL_REBUILDING_SNAP)
+	((zv->rebuild_info.zv_rebuild_status == ZVOL_REBUILDING_SNAP)	\
+	    || (zv->rebuild_info.zv_rebuild_status == ZVOL_REBUILDING_AFS))
 #define	ZVOL_IS_REBUILDED(zv)		\
 	(zv->rebuild_info.zv_rebuild_status == ZVOL_REBUILDING_DONE)
 #define	ZVOL_IS_REBUILDING_ERRORED(zv)	\
