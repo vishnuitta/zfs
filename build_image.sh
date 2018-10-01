24ba13ad25b3721daa2102f2127b02c48eece69a
Kiran Mova <kiran.mova@mayadata.io>
2018-10-01 20:07:45 +0530

    feat(build): build cstor-pool and cstor-base docker images (#125) (#126)
    * feat(build): build cstor-pool and cstor-base docker images (#125)
    
    Moving the code for building cstor images from openebs/cstor
    to this openebs/zfs repo. This will ensure that images are
    built per commit and CI can be run with these intermediate
    images
    
    Also enabled travis builds to run on release branch and
    release tags
    
    Signed-off-by: kmova <kiran.mova@openebs.io>
    (cherry picked from commit f760c32bf46bdc34f04209c6a061efeaec319bb6)

---
+++ build_image.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+BUILD_DATE=$(date +'%Y%m%d%H%M%S')
+REPO_NAME="openebs/cstor-base"
+
+mkdir -p ./docker/zfs/bin
+mkdir -p ./docker/zfs/lib
+
+cp cmd/zrepl/.libs/zrepl ./docker/zfs/bin
+cp cmd/zpool/.libs/zpool ./docker/zfs/bin
+cp cmd/zfs/.libs/zfs ./docker/zfs/bin
+
+cp lib/libzrepl/.libs/*.so* ./docker/zfs/lib
+cp lib/libzpool/.libs/*.so* ./docker/zfs/lib
+cp lib/libuutil/.libs/*.so* ./docker/zfs/lib
+cp lib/libnvpair/.libs/*.so* ./docker/zfs/lib
+cp lib/libzfs/.libs/*.so* ./docker/zfs/lib
+cp lib/libzfs_core/.libs/*.so* ./docker/zfs/lib
+
+sudo docker version
+sudo docker build --help
+
+echo "Build image ${REPO_NAME}:ci with BUILD_DATE=${BUILD_DATE}"
+cd docker && \
+ sudo docker build -f Dockerfile.base -t ${REPO_NAME}:ci --build-arg BUILD_DATE=${BUILD_DATE} . && \
+ IMAGE_REPO=${REPO_NAME} ./push && \
+ cd ..
+
+REPO_NAME="openebs/cstor-pool"
+echo "Build image ${REPO_NAME}:ci with BUILD_DATE=${BUILD_DATE}"
+cd docker && \
+ sudo docker build -f Dockerfile -t ${REPO_NAME}:ci --build-arg BUILD_DATE=${BUILD_DATE} . && \
+ IMAGE_REPO=${REPO_NAME} ./push && \
+ cd ..
+
+rm -rf ./docker/zfs
